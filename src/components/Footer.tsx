1b6b00edeac372867c5b6f30434fd978de3db043
César Augusto <97255173+oCesaum@users.noreply.github.com>
2023-04-19 07:07:32 -0300

    componentizing, finalizing the footer and small header changes

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+import { Socials } from "./Socials";
+
+export function Footer() {
+  return (
+    <footer className="py-5 px-20 2xl:px-28 text-sm 2xl:text-base flex items-center justify-between">
+      <Socials />
+      <div>© 2023 César Augusto</div>
+    </footer>
+  )
+}
