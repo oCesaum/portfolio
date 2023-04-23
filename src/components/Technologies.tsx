13c85cc7b8fd280e91115bb0039c736becca41fe
César Augusto <97255173+oCesaum@users.noreply.github.com>
2023-04-23 12:37:43 -0300

    adding Technologies name

--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -1,8 +1,9 @@
 export default function Technologies() {
   return (
-    <div className="grid grid-cols-3 sm:grid-cols-4 justify-items-center gap-5">
-      <div className="w-20 h-20 sm:w-24 sm:h-24 2xl:w-36 2xl:h-36 flex flex-col justify-center items-center shadow-md shadow-zinc-950 rounded-full hover:scale-110 transition">
-        <svg className="w-10 h-10 sm:w-14 sm:h-14 2xl:w-20 2xl:h-20" width={56} height={56} viewBox="0 0 128 128">
+    <div className="grid grid-cols-3 sm:grid-cols-4 justify-items-center gap-10">
+      <div className="w-24 h-24 2xl:w-36 2xl:h-36 flex flex-col justify-center items-center shadow-md shadow-zinc-950 rounded-full hover:scale-110 transition relative group">
+        <p className="absolute -top-5 text-sm font-bold text-[#e44d26] hidden group-hover:block">HTML5</p>
+        <svg className="w-14 h-14 2xl:w-20 2xl:h-20" width={56} height={56} viewBox="0 0 128 128">
           <path
             fill="#E44D26"
             d="M19.037 113.876L9.032 1.661h109.936l-10.016 112.198-45.019 12.48z"
@@ -21,8 +22,9 @@ export default function Technologies() {
           ></path>
         </svg>
       </div>
-      <div className="w-20 h-20 sm:w-24 sm:h-24 2xl:w-36 2xl:h-36 flex flex-col justify-center items-center shadow-md shadow-zinc-950 rounded-full hover:scale-110 transition">
-        <svg className="w-10 h-10 sm:w-14 sm:h-14 2xl:w-20 2xl:h-20" width={56} height={56} viewBox="0 0 128 128">
+      <div className="w-24 h-24 2xl:w-36 2xl:h-36 flex flex-col justify-center items-center shadow-md shadow-zinc-950 rounded-full hover:scale-110 transition relative group">
+        <p className="absolute -top-5 text-sm font-bold text-[#1572b6] hidden group-hover:block">CSS3</p>
+        <svg className="w-14 h-14 2xl:w-20 2xl:h-20" width={56} height={56} viewBox="0 0 128 128">
           <path
             fill="#1572B6"
             d="M18.814 114.123L8.76 1.352h110.48l-10.064 112.754-45.243 12.543-45.119-12.526z"
@@ -49,8 +51,9 @@ export default function Technologies() {
           ></path>
         </svg>
       </div>
-      <div className="w-20 h-20 sm:w-24 sm:h-24 2xl:w-36 2xl:h-36 flex flex-col justify-center items-center shadow-md shadow-zinc-950 rounded-full hover:scale-110 transition">
-        <svg className="w-10 h-10 sm:w-14 sm:h-14 2xl:w-20 2xl:h-20" width={56} height={56} viewBox="0 0 128 128">
+      <div className="w-24 h-24 2xl:w-36 2xl:h-36 flex flex-col justify-center items-center shadow-md shadow-zinc-950 rounded-full hover:scale-110 transition relative group">
+        <p className="absolute -top-5 text-sm font-bold text-[#f0db4f] hidden group-hover:block">Javascript</p>
+        <svg className="w-14 h-14 2xl:w-20 2xl:h-20" width={56} height={56} viewBox="0 0 128 128">
           <path fill="#F0DB4F" d="M1.408 1.408h125.184v125.185H1.408z"></path>
           <path
             fill="#323330"
@@ -58,15 +61,17 @@ export default function Technologies() {
           ></path>
         </svg>
       </div>
-      <div className="w-20 h-20 sm:w-24 sm:h-24 2xl:w-36 2xl:h-36 flex flex-col justify-center items-center shadow-md shadow-zinc-950 rounded-full hover:scale-110 transition">
-        <svg className="w-10 h-10 sm:w-14 sm:h-14 2xl:w-20 2xl:h-20" width={56} height={56} viewBox="0 0 128 128">
+      <div className="w-24 h-24 2xl:w-36 2xl:h-36 flex flex-col justify-center items-center shadow-md shadow-zinc-950 rounded-full hover:scale-110 transition relative group">
+        <p className="absolute -top-5 text-sm font-bold text-[#61dafb] hidden group-hover:block">React</p>
+        <svg className="w-14 h-14 2xl:w-20 2xl:h-20" width={56} height={56} viewBox="0 0 128 128">
           <g fill="#61DAFB">
             <circle cx="64" cy="64" r="11.4"></circle>
             <path d="M107.3 45.2c-2.2-.8-4.5-1.6-6.9-2.3.6-2.4 1.1-4.8 1.5-7.1 2.1-13.2-.2-22.5-6.6-26.1-1.9-1.1-4-1.6-6.4-1.6-7 0-15.9 5.2-24.9 13.9-9-8.7-17.9-13.9-24.9-13.9-2.4 0-4.5.5-6.4 1.6-6.4 3.7-8.7 13-6.6 26.1.4 2.3.9 4.7 1.5 7.1-2.4.7-4.7 1.4-6.9 2.3C8.2 50 1.4 56.6 1.4 64s6.9 14 19.3 18.8c2.2.8 4.5 1.6 6.9 2.3-.6 2.4-1.1 4.8-1.5 7.1-2.1 13.2.2 22.5 6.6 26.1 1.9 1.1 4 1.6 6.4 1.6 7.1 0 16-5.2 24.9-13.9 9 8.7 17.9 13.9 24.9 13.9 2.4 0 4.5-.5 6.4-1.6 6.4-3.7 8.7-13 6.6-26.1-.4-2.3-.9-4.7-1.5-7.1 2.4-.7 4.7-1.4 6.9-2.3 12.5-4.8 19.3-11.4 19.3-18.8s-6.8-14-19.3-18.8zM92.5 14.7c4.1 2.4 5.5 9.8 3.8 20.3-.3 2.1-.8 4.3-1.4 6.6-5.2-1.2-10.7-2-16.5-2.5-3.4-4.8-6.9-9.1-10.4-13 7.4-7.3 14.9-12.3 21-12.3 1.3 0 2.5.3 3.5.9zM81.3 74c-1.8 3.2-3.9 6.4-6.1 9.6-3.7.3-7.4.4-11.2.4-3.9 0-7.6-.1-11.2-.4-2.2-3.2-4.2-6.4-6-9.6-1.9-3.3-3.7-6.7-5.3-10 1.6-3.3 3.4-6.7 5.3-10 1.8-3.2 3.9-6.4 6.1-9.6 3.7-.3 7.4-.4 11.2-.4 3.9 0 7.6.1 11.2.4 2.2 3.2 4.2 6.4 6 9.6 1.9 3.3 3.7 6.7 5.3 10-1.7 3.3-3.4 6.6-5.3 10zm8.3-3.3c1.5 3.5 2.7 6.9 3.8 10.3-3.4.8-7 1.4-10.8 1.9 1.2-1.9 2.5-3.9 3.6-6 1.2-2.1 2.3-4.2 3.4-6.2zM64 97.8c-2.4-2.6-4.7-5.4-6.9-8.3 2.3.1 4.6.2 6.9.2 2.3 0 4.6-.1 6.9-.2-2.2 2.9-4.5 5.7-6.9 8.3zm-18.6-15c-3.8-.5-7.4-1.1-10.8-1.9 1.1-3.3 2.3-6.8 3.8-10.3 1.1 2 2.2 4.1 3.4 6.1 1.2 2.2 2.4 4.1 3.6 6.1zm-7-25.5c-1.5-3.5-2.7-6.9-3.8-10.3 3.4-.8 7-1.4 10.8-1.9-1.2 1.9-2.5 3.9-3.6 6-1.2 2.1-2.3 4.2-3.4 6.2zM64 30.2c2.4 2.6 4.7 5.4 6.9 8.3-2.3-.1-4.6-.2-6.9-.2-2.3 0-4.6.1-6.9.2 2.2-2.9 4.5-5.7 6.9-8.3zm22.2 21l-3.6-6c3.8.5 7.4 1.1 10.8 1.9-1.1 3.3-2.3 6.8-3.8 10.3-1.1-2.1-2.2-4.2-3.4-6.2zM31.7 35c-1.7-10.5-.3-17.9 3.8-20.3 1-.6 2.2-.9 3.5-.9 6 0 13.5 4.9 21 12.3-3.5 3.8-7 8.2-10.4 13-5.8.5-11.3 1.4-16.5 2.5-.6-2.3-1-4.5-1.4-6.6zM7 64c0-4.7 5.7-9.7 15.7-13.4 2-.8 4.2-1.5 6.4-2.1 1.6 5 3.6 10.3 6 15.6-2.4 5.3-4.5 10.5-6 15.5C15.3 75.6 7 69.6 7 64zm28.5 49.3c-4.1-2.4-5.5-9.8-3.8-20.3.3-2.1.8-4.3 1.4-6.6 5.2 1.2 10.7 2 16.5 2.5 3.4 4.8 6.9 9.1 10.4 13-7.4 7.3-14.9 12.3-21 12.3-1.3 0-2.5-.3-3.5-.9zM96.3 93c1.7 10.5.3 17.9-3.8 20.3-1 .6-2.2.9-3.5.9-6 0-13.5-4.9-21-12.3 3.5-3.8 7-8.2 10.4-13 5.8-.5 11.3-1.4 16.5-2.5.6 2.3 1 4.5 1.4 6.6zm9-15.6c-2 .8-4.2 1.5-6.4 2.1-1.6-5-3.6-10.3-6-15.6 2.4-5.3 4.5-10.5 6-15.5 13.8 4 22.1 10 22.1 15.6 0 4.7-5.8 9.7-15.7 13.4z"></path>
           </g>
         </svg>
       </div>
-      <div className="w-20 h-20 sm:w-24 sm:h-24 2xl:w-36 2xl:h-36 flex flex-col justify-center items-center shadow-md shadow-zinc-950 rounded-full hover:scale-110 transition">
+      <div className="w-24 h-24 2xl:w-36 2xl:h-36 flex flex-col justify-center items-center shadow-md shadow-zinc-950 rounded-full hover:scale-110 transition relative group">
+        <p className="absolute -top-5 text-sm font-bold text-[#38bdf8] hidden group-hover:block">Tailwind</p>
         <svg
           width={56}
           height={56}
@@ -84,8 +89,9 @@ export default function Technologies() {
           ></path>
         </svg>
       </div>
-      <div className="w-20 h-20 sm:w-24 sm:h-24 2xl:w-36 2xl:h-36 flex flex-col justify-center items-center shadow-md shadow-zinc-950 rounded-full hover:scale-110 transition">
-        <svg className="w-10 h-10 sm:w-14 sm:h-14 2xl:w-20 2xl:h-20" width={56} height={56} viewBox="0 0 128 128">
+      <div className="w-24 h-24 2xl:w-36 2xl:h-36 flex flex-col justify-center items-center shadow-md shadow-zinc-950 rounded-full hover:scale-110 transition relative group">
+        <p className="absolute -top-5 text-sm font-bold text-[#000] hidden group-hover:block">Github</p>
+        <svg className="w-14 h-14 2xl:w-20 2xl:h-20" width={56} height={56} viewBox="0 0 128 128">
           <path
             fillRule="evenodd"
             clipRule="evenodd"
@@ -94,8 +100,9 @@ export default function Technologies() {
           <path d="M26.484 91.806c-.133.3-.605.39-1.035.185-.44-.196-.685-.605-.543-.906.13-.31.603-.395 1.04-.188.44.197.69.61.537.91zm2.446 2.729c-.287.267-.85.143-1.232-.28-.396-.42-.47-.983-.177-1.254.298-.266.844-.14 1.24.28.394.426.472.984.17 1.255zM31.312 98.012c-.37.258-.976.017-1.35-.52-.37-.538-.37-1.183.01-1.44.373-.258.97-.025 1.35.507.368.545.368 1.19-.01 1.452zm3.261 3.361c-.33.365-1.036.267-1.552-.23-.527-.487-.674-1.18-.343-1.544.336-.366 1.045-.264 1.564.23.527.486.686 1.18.333 1.543zm4.5 1.951c-.147.473-.825.688-1.51.486-.683-.207-1.13-.76-.99-1.238.14-.477.823-.7 1.512-.485.683.206 1.13.756.988 1.237zm4.943.361c.017.498-.563.91-1.28.92-.723.017-1.308-.387-1.315-.877 0-.503.568-.91 1.29-.924.717-.013 1.306.387 1.306.88zm4.598-.782c.086.485-.413.984-1.126 1.117-.7.13-1.35-.172-1.44-.653-.086-.498.422-.997 1.122-1.126.714-.123 1.354.17 1.444.663zm0 0"></path>
         </svg>
       </div>
-      <div className="w-20 h-20 sm:w-24 sm:h-24 2xl:w-36 2xl:h-36 flex flex-col justify-center items-center shadow-md shadow-zinc-950 rounded-full hover:scale-110 transition">
-        <svg className="w-10 h-10 sm:w-14 sm:h-14 2xl:w-20 2xl:h-20" width={56} height={56} viewBox="0 0 128 128">
+      <div className="w-24 h-24 2xl:w-36 2xl:h-36 flex flex-col justify-center items-center shadow-md shadow-zinc-950 rounded-full hover:scale-110 transition relative group">
+        <p className="absolute -top-5 text-sm font-bold text-[#007acc] hidden group-hover:block">Typescript</p>
+        <svg className="w-14 h-14 2xl:w-20 2xl:h-20" width={56} height={56} viewBox="0 0 128 128">
           <path fill="#fff" d="M22.67 47h99.67v73.67H22.67z"></path>
           <path
             data-name="original"
@@ -104,8 +111,9 @@ export default function Technologies() {
           ></path>
         </svg>
       </div>
-      <div className="w-20 h-20 sm:w-24 sm:h-24 2xl:w-36 2xl:h-36 flex flex-col justify-center items-center shadow-md shadow-zinc-950 rounded-full hover:scale-110 transition">
-        <svg className="w-10 h-10 sm:w-14 sm:h-14 2xl:w-20 2xl:h-20" width={56} height={56} viewBox="0 0 128 128">
+      <div className="w-24 h-24 2xl:w-36 2xl:h-36 flex flex-col justify-center items-center shadow-md shadow-zinc-950 rounded-full hover:scale-110 transition relative group">
+        <p className="absolute -top-5 text-sm font-bold text-[#000] hidden group-hover:block">Nextjs</p>
+        <svg className="w-14 h-14 2xl:w-20 2xl:h-20" width={56} height={56} viewBox="0 0 128 128">
           <path d="M64 0C28.7 0 0 28.7 0 64s28.7 64 64 64c11.2 0 21.7-2.9 30.8-7.9L48.4 55.3v36.6h-6.8V41.8h6.8l50.5 75.8C116.4 106.2 128 86.5 128 64c0-35.3-28.7-64-64-64zm22.1 84.6l-7.5-11.3V41.8h7.5v42.8z"></path>
         </svg>
       </div>
