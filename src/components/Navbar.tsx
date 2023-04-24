7d6628b98289e3ea6345f68a7c2fae0b680b4b4f
César Augusto <97255173+oCesaum@users.noreply.github.com>
2023-04-24 18:35:49 -0300

    little close mobile menu button modification

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -48,7 +48,7 @@ const Navbar = () => {
     {mobileMenuIsOpen &&
       <div className="md:hidden fixed top-0 right-0 bg-zinc-950/70 w-screen h-screen z-10 transition-all cursor-pointer" onClick={(event) => closeMobileMenu(event)}>
         <div className="bg-zinc-950 w-fit h-full px-10 py-5 ml-auto cursor-default">
-          <button className="flex items-center justify-center ml-auto my-3 mr-3" onClick={() => setMobileMenuIsOpen(false)}>
+          <button className="flex items-center justify-center ml-auto my-3 mr-3 -translate-y-2/4" onClick={() => setMobileMenuIsOpen(false)}>
           <svg className="fill-zinc-300 w-12 h-12 p-3 transition-transform duration-300 hover:rotate-180" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
         </button>
           <nav className="flex flex-col items-center gap-16">
