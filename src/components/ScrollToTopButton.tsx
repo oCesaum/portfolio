ea963d440ffb5648e46819bfc8891353bdf1a659
César Augusto <97255173+oCesaum@users.noreply.github.com>
2023-04-25 09:05:38 -0300

    adding new back to top button

---
+++ src/components/ScrollToTopButton.tsx
@@ -0,0 +1,43 @@
+"use client"
+
+import { useState, useEffect } from 'react';
+
+const ScrollToTopButton: React.FC = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 300) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', toggleVisibility);
+    return () => window.removeEventListener('scroll', toggleVisibility);
+  }, []);
+
+  return (
+    <>
+      {isVisible && (
+        <button
+          onClick={scrollToTop}
+          className="fixed bottom-4 right-4 p-3 bg-zinc-800 hover:bg-zinc-950 text-white rounded-full transition-colors"
+          aria-label="Scroll to top"
+        >
+          <svg className='w-4 h-4 fill-white' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z"/></svg>
+        </button>
+      )}
+    </>
+  );
+};
+
+export default ScrollToTopButton;
