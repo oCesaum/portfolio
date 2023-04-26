5338cc121d9d90060784fc2b6ce0dd66e1b81e67
César Augusto <97255173+oCesaum@users.noreply.github.com>
2023-04-25 21:18:52 -0300

    updating Projects and Project components

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,25 +1,43 @@
-import Image from "next/image";
-import ProjectImage from "../../public/project-image.jpg"
+import Image, { StaticImageData } from "next/image";
 
-export default function Project () {
-return (
+interface ProjectProps {
+  projectName: string;
+  projectDescription: string;
+  projectLink: string;
+  projectImageUrl: StaticImageData;
+}
+
+const Project = ({
+  projectName,
+  projectDescription,
+  projectLink,
+  projectImageUrl,
+}: ProjectProps) => {
+  return (
     <div className="p-6 space-y-4 max-w-full md:p-4 md:space-y-2 md:min-w-[250px] lg:w-[360px] xl:w-[416px] xl:space-y-3 2xl:w-[624px] 2xl:p-6 2xl:space-y-4 rounded-lg shadow-sm hover:shadow-md shadow-zinc-950 hover:shadow-zinc-950">
         <Image
           className="rounded-md w-full"
-          src={ProjectImage}
-          alt="Imagem aleatória 1"
+          src={projectImageUrl}
+          alt={`Imagem do projeto ${projectName}`}
           priority
           width={1080}
+          height={325}
           quality={100}
         />
-        <h3 className="mx-auto uppercase text-center text-xl md:text-sm lg:text-base xl:text-lg 2xl:text-2xl w-fit">Nome do projeto</h3>
-        <p className="text-base md:text-sm 2xl:text-base">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Dignissimos, facilis quam molestias officiis error commodi possimus
-          illo ab quo praesentium maiores adipisci est quisquam dolores vitae
-          labore, suscipit deleniti nihil.
-        </p>
-        <a href="#" target="_blank" rel="noopener" className="inline-block px-6 md:px-4 lg:px-6 py-2 rounded-full shadow-lg bg-zinc-800 shadow-zinc-950 text-base md:text-sm 2xl:text-base text-center transition-transform hover:scale-110">Ver projeto</a>
+      <h3 className="mx-auto uppercase text-center text-xl md:text-sm lg:text-base xl:text-lg 2xl:text-2xl w-fit">
+        {projectName}
+      </h3>
+      <p className="text-base md:text-sm 2xl:text-base">{projectDescription}</p>
+      <a
+        href={projectLink}
+        target="_blank"
+        rel="noopener"
+        className="inline-block px-6 md:px-4 lg:px-6 py-2 rounded-full shadow-lg bg-zinc-800 shadow-zinc-950 text-base md:text-sm 2xl:text-base text-center transition-transform hover:scale-110"
+      >
+        Ver projeto
+      </a>
     </div>
   );
 };
+
+export default Project;
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,12 +1,47 @@
 import Project from "./Project";
 
+import TailwindSpotify from "../../public/tailwind-spotify.png"
+import Image from "../../public/project-image.jpg"
+
 export default function Projects() {
+  const projects = [
+    {
+      projectName: 'Tailwind Spotify',
+      projectDescription: 'O Tailwind Spotify é um projeto para aprimoramento das minhas habilidades em Tailwind, fazendo assim um clone do Spotify. O projeto ainda não está concluído, mas clique no link para ver como está ficando.',
+      projectLink: 'https://tailwind-spotify-phi.vercel.app/',
+      projectImageUrl: TailwindSpotify,
+    },
+    {
+      projectName: 'Projeto 2',
+      projectDescription: 'Descrição do projeto 2... Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic nobis repellat adipisci, in, autem debitis commodi ab rerum, culpa natus inventore molestias distinctio ipsa assumenda libero quisquam fugiat modi obcaecati.',
+      projectLink: '/#projects',
+      projectImageUrl: Image,
+    },
+    {
+      projectName: 'Projeto 3',
+      projectDescription: 'Descrição do projeto 2... Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic nobis repellat adipisci, in, autem debitis commodi ab rerum, culpa natus inventore molestias distinctio ipsa assumenda libero quisquam fugiat modi obcaecati.',
+      projectLink: '/#projects',
+      projectImageUrl: Image,
+    },
+    {
+      projectName: 'Projeto 4',
+      projectDescription: 'Descrição do projeto 2... Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic nobis repellat adipisci, in, autem debitis commodi ab rerum, culpa natus inventore molestias distinctio ipsa assumenda libero quisquam fugiat modi obcaecati.',
+      projectLink: '/#projects',
+      projectImageUrl: Image,
+    },
+  ];
+
   return (
     <div className="md:grid md:grid-cols-2 gap-0 md:gap-10 lg:gap-20 mx-auto w-fit">
-      <Project />
-      <Project />
-      <Project />
-      <Project />
+       {projects.map((project) => (
+        <Project
+          key={project.projectName}
+          projectName={project.projectName}
+          projectDescription={project.projectDescription}
+          projectLink={project.projectLink}
+          projectImageUrl={project.projectImageUrl}
+        />
+      ))}
     </div>
   );
 };
