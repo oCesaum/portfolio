ed440e8b03029bf7a13fce246484d4433782b10d
César Augusto <97255173+oCesaum@users.noreply.github.com>
2023-07-23 11:45:27 -0300

    improving Navbar mobile menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,8 @@ import { useState } from "react";
 import { Link } from "react-scroll";
 
 const Navbar = () => {
-  const [mobileMenuIsOpen, setMobileMenuIsOpen] = useState(false);
+  const [handleActivateMobileMenu, setHandleActivateMobileMenu] =
+    useState(false);
 
   return (
     <>
@@ -42,41 +43,20 @@ const Navbar = () => {
           </li>
         </ul>
       </nav>
-
-      <div className="block lg:hidden">
-
-        {/* melhorar botão de abrir e fechar menu mobile, fazer ser apenas 1 botão */}
-        <button
-          aria-label="Ativar menu mobile"
-          onClick={() => setMobileMenuIsOpen(true)}
-        >
-          <svg
-            className="h-6 w-6 fill-slate-950"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 448 512"
-          >
-            <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
-          </svg>
-        </button>
-      </div>
+      <button
+        aria-label="Ativar menu mobile"
+        onClick={() => setHandleActivateMobileMenu(!handleActivateMobileMenu)}
+        className={`${
+          handleActivateMobileMenu
+            ? "fixed right-6 top-6 border-t-transparent before:rotate-[135deg] after:-rotate-[135deg] before:top-1"
+            : "relative rotate-180 border-t-2 border-t-slate-950"
+        }  z-40 lg:hidden h-4 w-6 after:w-6 after:h-0.5 after:bg-slate-950 after:block after:absolute after:top-1/2 after:-translate-y-full before:w-6 before:h-0.5 before:bg-slate-950 before:block before:absolute before:bottom-0 transition-transform duration-500`}
+      ></button>
       <div
-        className={`fixed right-0 top-0 z-50 h-screen w-fit shadow-lg cursor-default bg-slate-100 px-10 py-5 transition-all duration-300 ease-in lg:hidden ${
-          mobileMenuIsOpen ? "" : "translate-x-[110%]"
+        className={`fixed right-0 top-0 z-30 h-screen w-fit shadow-lg cursor-default bg-slate-100 px-10 py-5 transition-all duration-300 ease-in lg:hidden ${
+          handleActivateMobileMenu ? "" : "translate-x-[110%]"
         }`}
       >
-        <button
-          className="my-2 ml-auto flex -translate-y-2/4"
-          onClick={() => setMobileMenuIsOpen(false)}
-          aria-label="Desativar menu mobile"
-        >
-          <svg
-            className="h-12 w-12 fill-slate-950 p-3 transition-transform duration-300 hover:rotate-180"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 384 512"
-          >
-            <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
-          </svg>
-        </button>
         <nav>
           <ul className="flex flex-col items-center gap-16">
             <li>
