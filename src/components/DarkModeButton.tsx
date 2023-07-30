b7b99b8356ae353669eb484985cc30d976b649c4
César Augusto <97255173+oCesaum@users.noreply.github.com>
2023-07-30 16:17:31 -0300

    fixing window error

--- src/components/DarkModeButton.tsx
+++ src/components/DarkModeButton.tsx
@@ -4,15 +4,29 @@ import { Moon, Sun } from "lucide-react";
 import { useEffect, useState } from "react";
 
 export default function DarkModeButton() {
-  const prefersDarkMode = window.matchMedia(
-    "(prefers-color-scheme: dark)"
-  ).matches ?? false;
+  const [prefersDarkMode, setPrefersDarkMode] = useState(false);
   const [theme, setTheme] = useState(() => {
     const storedTheme = localStorage.getItem("theme");
     return storedTheme || (prefersDarkMode ? "dark" : "light");
   });
-
   useEffect(() => {
+    if (typeof window !== "undefined") {
+      const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+      setPrefersDarkMode(mediaQuery.matches);
+
+      const handleChange = (event: {
+        matches: boolean | ((prevState: boolean) => boolean);
+      }) => {
+        setPrefersDarkMode(event.matches);
+      };
+
+      mediaQuery.addEventListener("change", handleChange);
+
+      return () => {
+        mediaQuery.removeEventListener("change", handleChange);
+      };
+    }
+
     document.documentElement.classList.toggle("dark", theme === "dark");
     localStorage.setItem("theme", theme);
   }, [theme]);
