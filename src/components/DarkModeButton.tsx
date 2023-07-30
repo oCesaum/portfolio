d52123ffdba7a0045fe9de1674c73ac3a9b319d5
César Augusto <97255173+oCesaum@users.noreply.github.com>
2023-07-30 16:28:05 -0300

    updating localStorage theme verify

--- src/components/DarkModeButton.tsx
+++ src/components/DarkModeButton.tsx
@@ -4,7 +4,7 @@ import { Moon, Sun } from "lucide-react";
 import { useEffect, useState } from "react";
 
 export default function DarkModeButton() {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState(localStorage.theme ?? "light");
 
   useEffect(() => {
     document.documentElement.classList.toggle("dark", theme === "dark");
