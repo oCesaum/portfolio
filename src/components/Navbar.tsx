b01dac64d4f0c54ca520d00a47df62f44d3ea8fa
César Augusto <97255173+oCesaum@users.noreply.github.com>
2023-08-06 10:49:24 -0300

    fixing header nav visual bugs

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -47,11 +47,7 @@ const Navbar = () => {
       <button
         aria-label="Ativar e desativar menu mobile"
         onClick={() => setHandleActivateMobileMenu(!handleActivateMobileMenu)}
-        className={`${
-          handleActivateMobileMenu
-            ? "fixed right-6 top-6 border-t-transparent before:rotate-[135deg] after:-rotate-[135deg] before:top-1 after:-translate-y-[200%]"
-            : "relative rotate-180 border-t-2 border-t-slate-950 dark:border-t-slate-200 after:-translate-y-full"
-        }  z-40 lg:hidden h-4 w-6 after:w-6 after:h-0.5 after:bg-slate-950 dark:after:bg-slate-200 after:block after:absolute after:top-1/2 before:w-6 before:h-0.5 before:bg-slate-950 dark:before:bg-slate-200 before:block before:absolute before:bottom-0 transition-transform duration-500`}
+        className="relative border-t-2 border-t-slate-950 dark:border-t-slate-200 after:-translate-y-full lg:hidden h-4 w-6 after:w-6 after:h-0.5 after:bg-slate-950 dark:after:bg-slate-200 after:block after:absolute after:top-1/2 before:w-6 before:h-0.5 before:bg-slate-950 dark:before:bg-slate-200 before:block before:absolute before:bottom-0 transition-transform duration-500"
       ></button>
       <div
         className={`fixed right-0 top-0 z-30 h-screen w-fit shadow-lg cursor-default bg-slate-100 dark:bg-slate-900 px-10 py-5 transition-all duration-300 ease-in lg:hidden ${
@@ -59,6 +55,13 @@ const Navbar = () => {
         }`}
       >
         <nav>
+          <button
+            aria-label="Ativar e desativar menu mobile"
+            onClick={() =>
+              setHandleActivateMobileMenu(!handleActivateMobileMenu)
+            }
+            className="fixed right-6 top-6 before:rotate-[135deg] after:-rotate-[135deg] before:top-[0.35rem] after:-translate-y-full z-40 lg:hidden h-4 w-6 after:w-6 after:h-0.5 after:bg-slate-950 dark:after:bg-slate-200 after:block after:absolute after:top-1/2 before:w-6 before:h-0.5 before:bg-slate-950 dark:before:bg-slate-200 before:block before:absolute before:bottom-0 transition-transform duration-500"
+          ></button>
           <ul className="flex flex-col items-center gap-16">
             <li>
               <Link
