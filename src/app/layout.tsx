07d044eed04ac050a11964325df285efea6e5fad
oCesaum <cesaraugustopinho5@gmail.com>
2025-12-08 14:20:40 -0300

    Add Facebook Pixel tracking script to RootLayout component for enhanced analytics and user tracking.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Poppins } from "next/font/google";
+import Script from "next/script";
 
 import "./globals.css";
 import "./scrollbar.css";
@@ -21,9 +22,34 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br">
+      <head>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=2062041987533580&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+      </head>
       <body
         className={`mx-auto max-w-[1600px] bg-slate-200 dark:bg-slate-800 transition-all duration-500 ${poppins.className}`}
       >
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '2062041987533580');
+            fbq('track', 'PageView');
+          `}
+        </Script>
         {children}
       </body>
     </html>
