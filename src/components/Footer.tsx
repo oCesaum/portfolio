1e59e13dc6f76a2fc79bcb024298889bf33619a7
oCesaum <cesaraugustopinho5@gmail.com>
2025-12-08 11:54:27 -0300

    Update Footer component copyright year from 2023 to 2026

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="flex flex-col items-center justify-between gap-5 px-6 py-5 text-sm sm:flex-row 2xl:px-28 2xl:text-base">
       <Socials componentType="footer" />
-      <div>&copy; 2023 César Augusto</div>
+      <div>&copy; 2026 César Augusto</div>
     </footer>
   );
 }
